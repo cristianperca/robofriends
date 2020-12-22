94c66155c830c9cb84d29b3542c129a83c1092ee
Cristian Perca <cristianperca@yahoo.com>
2020-12-22 07:02:05 +0100

    Add state - convert App component to class

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function SearchBox() {
+const SearchBox = ({ searchChange }) => {  
   return (
     <div className="pa2">
       <input className="pa3 ba b--green bg-lightest-blue"
         type="search"
-        placeholder="search ronots"
+        placeholder="search robots"
+        onChange={searchChange}
       />
     </div>
   )
