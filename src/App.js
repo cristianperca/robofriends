68ff3da89964f6cb2e1198174d63b0d6713af753
Cristian Perca <cristianperca@yahoo.com>
2020-12-22 07:38:21 +0100

    Fetch user list from jsonplaceholder API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, {Component } from 'react'
 import CardList from "./CardList"
-import {robots} from "./robots.js"
 import SearchBox from "./SearchBox"
-
+import "./App.css"
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}))
   }
 
   onSearchChange = (event) => {
@@ -20,13 +25,18 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
     })
-    return (
-      <div className="tc">
-        <h1>Robo friends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />,
-      </div>
-    )
+
+    if (this.state.robots.length === 0) {
+      return <h1>Loading ...</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />,
+        </div>
+      )
+    }
   }
 }
 
